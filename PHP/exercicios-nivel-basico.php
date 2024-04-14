16f3bd44391a1bfc44da6369df69f79a19a32534
krhuhun <jorgelisboa659@gmail.com>
2024-04-14 00:09:08 -0300

    exercicios

---
+++ PHP/exercicios-nivel-basico.php
@@ -0,0 +1,24 @@
+<?php
+
+// 1. Crie uma função que veja se um número é primo ou não
+// Um número primo é um número que só pode ser dividido por 1 e por ele mesmo
+
+
+// 2. Crie uma função que receba um nome retorne a quantidade de letras que ele tem
+// Exemplo: nome("João") deve retornar 4
+
+
+// 3. Crie uma função que receba um array de números e retorne a média deles
+// Exemplo: media([1,2,3,4,5]) deve retornar 3
+
+
+// 4. Crie uma função que receba um array de números e retorne o maior número
+// Exemplo: maior([1,2,3,4,5]) deve retornar 5
+
+
+// 5. Crie uma função que receba um array de números e retorne o menor número
+// Exemplo: menor([1,2,3,4,5]) deve retornar 1
+
+
+// 6. Crie uma função que receba um array de números e retorne a quantidade de números pares
+// Exemplo: pares([1,2,3,4,5]) deve retornar 2, pois há 2 números pares no array sendo eles 2 e 4
---
+++ PHP/exercicios-nivel-medio.php
@@ -0,0 +1,30 @@
+<?php
+
+// Crie uma função que recebe uma pessoa e retorna se ela é maior de idade ou não
+// A função deve receber um array associativa com o nome e a idade da pessoa
+// Se a pessoa tiver 18 anos ou mais, a função deve retornar "Maior de idade"
+// Se a pessoa tiver menos de 18 anos, a função deve retornar "Menor de idade"
+// Use a pessoa abaixo para testar a função:
+$pessoa = ['nome' => 'João', 'idade' => 20];
+
+
+// Crie uma função que "envie um email" para uma pessoa convidando ela para um evento
+// Use o array associativo da pessoa abaixo para enviar o email
+$conviteInfo = [
+    'nome' => 'Maria', 
+    'email' => 'maria@email.com', 
+    'evento' => 'Festa de aniversário', 
+    'data' => '01/01/2022',
+    'horario' => '20:00',
+    'local' => 'Rua da festa, 123',
+];
+// A função deve receber o array associativo com as informações do convite e printar um texto no seguinte formato:
+// "Olá, Maria! Você está convidado para a Festa de aniversário que acontecerá no dia 01/01/2022 às 20:00 no endereço Rua da festa, 123. Até lá!"
+
+// Crie uma função que recebe uma array de pessoas e da um "oi" para cada pessoa
+// A função deve receber um array de arrays associativos com o nome e a idade de cada pessoa
+// A função deve printar um texto para cada pessoa no seguinte formato:
+// "Olá, João! Você tem 20 anos."
+// "Olá, Maria! Você tem 30 anos."
+
+
